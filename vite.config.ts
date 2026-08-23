5b1b026731f9eee0c4736fbfd9ef9486fe5b834d
limsorn9 <limsorn9@gmail.com>
2026-08-23 13:04:06 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ command }) => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/PhnomPom/', // កំណត់ path សម្រាប់ GitHub Pages
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
