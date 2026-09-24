b2680b54dec52007d677127791901b476dff6c41
limsorn9 <limsorn9@gmail.com>
2026-09-24 10:30:34 +0700

    fix: restore missing getRetentionConfig and performRetentionCleanup imports in SchoolContext

--- src/context/SchoolContext.tsx
+++ src/context/SchoolContext.tsx
@@ -1,3 +1,4 @@
+import { getRetentionConfig, performRetentionCleanup } from '../utils/activityTracker';
 import React, { createContext, useContext, useState, useEffect, useRef, useCallback, useMemo } from 'react';
 
 import {
